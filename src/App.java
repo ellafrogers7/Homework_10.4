50caa5555c9d22f63e63b70793c7fd1ec04ea366
ellafrogers7 <113638056+ellafrogers7@users.noreply.github.com>
2022-10-04 13:15:12 -0700

    Update lines in functiosn

--- src/App.java
+++ src/App.java
@@ -1,9 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception 
+  public static void main(String[] args) throws Exception 
     {
         method1();
         method2();
         method3();
+        method4();
+        method5();
+        method6();
+        method7();
+        method8();
     }
 
     public static void method1()
@@ -11,34 +16,56 @@ public class App {
         System.out.println("*****");
         System.out.println("*****");
         System.out.println(" * *");
-        System.out.println("  *");
-        System.out.println(" * *");
-        System.out.println("\n");
+
         
         
     }
 
     public static void method2()
     {
-        System.out.println("*****");
-        System.out.println("*****");
-        System.out.println(" * *");
         System.out.println("  *");
         System.out.println(" * *");
-        System.out.println("*****");
-        System.out.println("*****");
         System.out.println("\n");
+        
     }
 
     public static void method3()
     {
-        System.out.println("  *");
-        System.out.println("  *");
-        System.out.println("  *");
         System.out.println("*****");
         System.out.println("*****");
         System.out.println(" * *");
+        
+    }
+    public static void method4()
+    {
         System.out.println("  *");
         System.out.println(" * *");
+        System.out.println("*****");
+        
+    }
+    public static void method5()
+    {
+        System.out.println("*****");
+        System.out.println("\n");
+        System.out.println("  *");
+       
+    }
+    public static void method6()
+        
+    {
+        System.out.println("  *");
+        System.out.println("  *");
+        System.out.println("*****");
+    }
+    public static void method7()
+    {
+        System.out.println("*****");
+        System.out.println(" * *");
+        System.out.println("  *");
+       
+    }
+    public static void method8()
+    {
+     System.out.println(" * *");
     }
 }
